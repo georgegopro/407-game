b56fbfa7c4bf5c4599742bad5920491ad895aefe
Stacking <547949141@qq.com>
2017-03-03 18:17:45 -0800

    update for the game
    create navigation and ball mover direction

--- game.js
+++ game.js
@@ -44,3 +44,105 @@ for(c=0; c<brickColumnCount; c++) {
         bricks[c][r] = { x: 0, y: 0, status: 1 };
     }
 }
+
+
+//添加监听事件
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function newGame()
+{
+    document.getElementById("score").innerHTML = 0;
+    document.location.reload();
+    
+}
+
+
+function keyDownHandler(e) {
+    if(e.keyCode === 39) {
+        rightPressed = true;
+    }
+    else if(e.keyCode === 37) {
+        leftPressed = true;
+    }
+}
+function keyUpHandler(e) {
+    if(e.keyCode === 39) {
+        rightPressed = false;
+    }
+    else if(e.keyCode === 37) {
+        leftPressed = false;
+    }
+}
+
+//画球
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, 6, 0, Math.PI*2);
+    ctx.fillStyle = "#000000";
+    ctx.fill();
+    ctx.closePath();
+}
+
+//背景记分器
+function drawScore() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Score: "+score, 8, 20);
+}
+
+//挡板构成
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#95DD00";
+    ctx.fill();
+    ctx.closePath();
+}
+
+//砖块儿图
+function drawBricks() {
+    for(c=0; c<brickColumnCount; c++) {
+        for(r=0; r<brickRowCount; r++) {
+            if(bricks[c][r].status === 1) {
+                var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+                var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                //砖块颜色
+                ctx.fillStyle = "#DD4800";
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+}
+
+//碰撞检测testing
+function collisionDetection() {
+    //循环便利二维数组
+    for(c=0; c<brickColumnCount; c++) {
+        for(r=0; r<brickRowCount; r++) {
+            var b = bricks[c][r];
+            if(b.status === 1) {
+                //如果碰撞,推算结果
+                if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+                    //更改方向
+                    dy = -dy;
+                    //砖块消失
+                    b.status = 0;
+                    //record score分数
+                    score++;
+                    document.getElementById("score").innerHTML = score;
+                    //判断输赢
+                    if(score === brickRowCount*brickColumnCount) {
+                        alert("YOU WIN, CONGRATULATIONS!");
+                        document.location.reload();
+                    }
+                }
+            }
+        }
+    }
+}
